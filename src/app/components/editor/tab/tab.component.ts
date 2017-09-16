0aa7733c2bb6b08815a9235310d758ea35ebcc1b
happou31 <happo31@gmail.com>
2017-09-17 02:05:32 +0900

    add jsdoc.

--- src/app/components/editor/tab/tab.component.ts
+++ src/app/components/editor/tab/tab.component.ts
@@ -23,6 +23,12 @@ export class TabComponent implements AfterViewInit {
 
     }
 
+    /**
+     * Tab element click event.
+     *
+     * @param {number} index clicked tab's index.
+     * @memberof TabComponent
+     */
     tabClick(index: number) {
         if (this.activeIndex === index) {
             return;
@@ -30,6 +36,13 @@ export class TabComponent implements AfterViewInit {
         this.toggleActive(index);
     }
 
+    /**
+     * Add tab button click event.
+     *
+     * @param {string} [fileName=`noname-${this.tabCount}`] default tab name.
+     * @param {string} [content=''] default tab content.
+     * @memberof TabComponent
+     */
     addTab(fileName = `noname-${this.tabCount}`, content = '') {
         const newTab = new TabModel();
         if (this.tabs.some(v => v.fileName === fileName)) {
@@ -42,17 +55,37 @@ export class TabComponent implements AfterViewInit {
         this.toggleActive(this.tabs.length - 1);
     }
 
+    /**
+     * Tab's name double click event.
+     *
+     * @param {number} index tab's index.
+     * @param {HTMLInputElement} element focus target <input>.
+     * @memberof TabComponent
+     */
     editTab(index: number, element: HTMLInputElement) {
         this.tabs[index].editing = true;
         setTimeout(() => element.focus(), 0);
     }
 
+    /**
+     * Tab's name input key event.
+     *
+     * @param {number} index tab's index.
+     * @param {KeyboardEvent} event keyboard event.
+     * @memberof TabComponent
+     */
     editInputKeyPress(index: number, event: KeyboardEvent) {
         if (event.key === 'Enter') {
             this.tabs[index].editing = false;
         }
     }
 
+    /**
+     * Tab's remove button event.
+     *
+     * @param {number} index tab's index.
+     * @memberof TabComponent
+     */
     removeTab(index: number) {
         this.tabs.splice(index, 1);
         if (this.activeIndex >= this.tabs.length) {
@@ -63,6 +96,12 @@ export class TabComponent implements AfterViewInit {
         }
     }
 
+    /**
+     * Change active tab event.
+     *
+     * @param {number} activateIndex activation tab index.
+     * @memberof TabComponent
+     */
     toggleActive(activateIndex: number) {
         this.activeIndex = activateIndex;
         this.tabs.forEach(v => v.isActive = false);
@@ -74,11 +113,21 @@ export class TabComponent implements AfterViewInit {
         console.log('active tab:', this.activeIndex);
     }
 
+    /**
+     * Active tab next.
+     *
+     * @memberof TabComponent
+     */
     tabNext() {
         const activeIndex = this.tabs.length >= this.activeIndex + 1 ? 0 : this.activeIndex + 1;
         this.toggleActive(activeIndex);
     }
 
+    /**
+     * Active tab back.
+     *
+     * @memberof TabComponent
+     */
     tabBack() {
         const activeIndex = this.activeIndex - 1 < 0 ? this.tabs.length - 1 : this.activeIndex - 1;
         this.toggleActive(activeIndex);
