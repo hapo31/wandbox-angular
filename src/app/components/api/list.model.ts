7f8618b8fcccaaa69455a1d9697f2ce1d6cf3074
happou31 <happo31@gmail.com>
2017-09-18 22:46:55 +0900

    Add api response interface

---
+++ src/app/components/api/list.model.ts
@@ -0,0 +1,28 @@
+export interface ListResponse {
+    'hoge-fuga': string;
+}
+
+export interface CompilerInfo {
+    'compiler-option-raw': boolean;
+    'runtime-option-raw': boolean;
+    'display-compile-command': string;
+    switched: CompilerFlagInfo[] | CompilerFlagSelectionInfo[];
+}
+
+export interface CompilerFlagInfo {
+    default: boolean;
+    name: string;
+    'display-flags': string;
+    'display-name': string;
+}
+
+export interface CompilerFlagSelectionInfo {
+    default: string;
+    options: CompilerFlagSelectionItemInfo[];
+}
+
+export interface CompilerFlagSelectionItemInfo {
+    name: string;
+    'display-flags': string;
+    'display-name': string;
+}
