0098bc788c2b0ac0e8c23fc16e950c69f90f4f25
happou31 <happo31@gmail.com>
2017-09-23 02:27:50 +0900

    Defined compile API param.

---
+++ src/app/components/api/compile.model.ts
@@ -0,0 +1,23 @@
+export interface CompileRequest {
+    compiler: string;
+    code: string;
+    codes: string[];
+    options: string;
+    stdin: string;
+    save: boolean;
+    'compiler-option-raw': string;
+    'runtime-option-raw': string;
+}
+
+export interface CompileResponse {
+    status: string;
+    signal: string;
+    compiler_output: string;
+    compiler_error: string;
+    compiler_message: string;
+    program_output: string;
+    program_error: string;
+    program_message: string;
+    permlink?: string;
+    url?: string;
+}
