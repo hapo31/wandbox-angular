558dcfb0e438c6eff2a588792b4c768f224367ec
happou31 <happo31@gmail.com>
2017-10-25 22:23:18 +0900

    Remove unused code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
-import { EditorComponent } from './components/editor/editor.component';
+import { Component } from '@angular/core';
 import { CompilerComponent } from './components/compiler/compiler.component';
-import { CheckboxOption, CompilerOptionModel, TextAreaOption, SelectBoxOption } from './components/compiler/compiler.model';
 import { RunCompileService } from './components/common/run-compile.service';
-import { CompileRequest } from './components/api/compile.model';
-
-import { LocalStorageService } from './components/common/local-storage.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    providers: [RunCompileService ]
+    providers: [RunCompileService]
 })
 export class AppComponent {
-    @ViewChild(EditorComponent) editorComponent: EditorComponent;
-    @ViewChild(CompilerComponent) compilerComponent: CompilerComponent;
-
-    constructor(private compile: RunCompileService, storage: LocalStorageService) { }
 }
