8864d59286d9def61a2b914302b58439eeb2d58e
happou31 <happo31@gmail.com>
2017-10-25 21:33:03 +0900

    Add fetch api.

--- src/app/components/api/permlink.service.ts
+++ src/app/components/api/permlink.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs/Subject';
 import { CompileResponse } from './compile.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class PermlinkService {
 
     subject = new Subject<CompileResponse>();
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private http: HttpClient, private route: ActivatedRoute) {
+        this.route.params
+            .flatMap(linkId => {
+                return this.http.get(environment.baseApiUrl + 'permlink/' + linkId);
+            })
+            .subscribe((res: CompileResponse) => {
+                this.subject.next(res);
+            });
+    }
 
+    public changePermlink$() {
+        return this.subject.asObservable();
     }
 }
