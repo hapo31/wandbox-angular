684706ce9ea8dc86fa9efa678b38c1b66a11c67c
happou31 <happo31@gmail.com>
2017-11-01 04:18:14 +0900

    Implements check permlink source data method.

--- src/app/components/api/permlink.service.ts
+++ src/app/components/api/permlink.service.ts
@@ -5,39 +5,58 @@ import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 import { Subject } from 'rxjs/Subject';
 
-import { CompileResponse } from './compile.model';
+import { PermlinkResponse } from './permlink.model';
 import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class PermlinkService {
+    private _loadedPermLink = false;
+    private _fetching = false;
+    private _resultCache: PermlinkResponse = null;
+    private _fetchObserver: Observable<PermlinkResponse> = null;
 
-    private subject = new Subject<CompileResponse>();
+    private subject = new Subject<PermlinkResponse>();
 
     constructor(private http: HttpClient, private route: ActivatedRoute) {
-        this.checkPermlink$().subscribe(res => {
-            this.subject.next(res);
-        });
+
     }
 
-    public changePermlink$() {
-        return this.subject.asObservable();
+    get requested() {
+        return this._loadedPermLink;
     }
 
-    private checkPermlink$(): Observable<CompileResponse> {
-        return Observable.create((observer: Observer<CompileResponse>) => {
-            const match = location.href.match(/.*\/permlink\/(.*)\/?/);
-            if (match && match.length) {
-                const linkId: string = match[1];
-                this.http.get(environment.baseApiUrl + 'permlink/' + linkId)
-                    .subscribe((res: CompileResponse) => {
-                        observer.next(res);
-                        observer.complete();
-                    },
-                    (err) => {
-                        observer.error(err);
-                    });
-            }
-        });
+    public get checkPermlink$(): Observable<PermlinkResponse> {
+        const match = location.href.match(/.*\/permlink\/(.*)\/?/);
+        this._loadedPermLink = !!(match && match.length);
+        console.log(this._loadedPermLink);
+        if (this._fetching) {
+            return this._fetchObserver;
+        } else {
+            return this._fetchObserver = Observable.create((observer: Observer<PermlinkResponse>) => {
+                if (this._loadedPermLink) {
+                    if (this._resultCache != null) {
+                        console.log('[PermlinkService]', 'cache subscribe');
+                        observer.next(this._resultCache);
+                    } else {
+                        const linkId: string = match[1];
+                        this._loadedPermLink = true;
+                        this._fetching = true;
+                        console.log('[PermlinkService]', 'fetch permlink');
+                        this.http.get(environment.baseApiUrl + 'permlink/' + linkId)
+                            .subscribe((res: PermlinkResponse) => {
+                                this._resultCache = res;
+                                this._fetching = false;
+                                console.log('[PermlinkService]', 'subscribe permlink');
+                                observer.next(res);
+                                observer.complete();
+                            },
+                            (err) => {
+                                observer.error(err);
+                            });
+                    }
+                }
+            });
+        }
     }
 
 }
