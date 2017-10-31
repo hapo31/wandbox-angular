acd2a94683a5aa72d5880b17e3f04a7f76a023b9
happou31 <happo31@gmail.com>
2017-10-31 23:27:18 +0900

    Implements parse url param method.

--- src/app/components/api/permlink.service.ts
+++ src/app/components/api/permlink.service.ts
@@ -1,28 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 import { Subject } from 'rxjs/Subject';
+
 import { CompileResponse } from './compile.model';
 import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class PermlinkService {
 
-    subject = new Subject<CompileResponse>();
+    private subject = new Subject<CompileResponse>();
 
     constructor(private http: HttpClient, private route: ActivatedRoute) {
-        this.route.params
-            .do(v => console.log('[PermlinkService]', v))
-            .filter(linkId => linkId && linkId.id != null)
-            .flatMap(linkId => {
-                return this.http.get(environment.baseApiUrl + 'permlink/' + linkId);
-            })
-            .subscribe((res: CompileResponse) => {
-                this.subject.next(res);
-            });
+        this.checkPermlink$().subscribe(res => {
+            this.subject.next(res);
+        });
     }
 
     public changePermlink$() {
         return this.subject.asObservable();
     }
+
+    private checkPermlink$(): Observable<CompileResponse> {
+        return Observable.create((observer: Observer<CompileResponse>) => {
+            const match = location.href.match(/.*\/permlink\/(.*)\/?/);
+            if (match && match.length) {
+                const linkId: string = match[1];
+                this.http.get(environment.baseApiUrl + 'permlink/' + linkId)
+                    .subscribe((res: CompileResponse) => {
+                        observer.next(res);
+                        observer.complete();
+                    },
+                    (err) => {
+                        observer.error(err);
+                    });
+            }
+        });
+    }
+
 }
